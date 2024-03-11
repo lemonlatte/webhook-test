49e35f1525bb765b9dd1c4b4c8fea253c7dc37f7
Jim Yeh <lemonlatte@gmail.com>
2024-03-11 08:12:43 +0800

    Add pydantic model for webhook

--- api/index.py
+++ api/index.py
@@ -1,19 +1,21 @@
 import os
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
 
 CLIENT_TOKEN = os.getenv("WEBHOOK_TOKEN")
 
+class WebhookPayload(BaseModel):
+    clientToken: str
+    secret: str
+
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
 
 @app.post("/api/webhook")
-def webhook(
-    clientToken: str,
-    secret: str
-):
-    if clientToken != CLIENT_TOKEN:
+def webhook(payload: WebhookPayload):
+    if payload.clientToken != CLIENT_TOKEN:
         raise ValueError("Invalid client token")
-    return secret
+    return payload.secret
